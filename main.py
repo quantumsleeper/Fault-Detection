2d3920228be2329b7cd4568b9f85bd8960d268c4
quantumsleeper <f2015511p@alumni.bits-pilani.ac.in>
2024-05-19 03:50:45 +0530

    added logger.py and exception.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from sensor.exception import SensorException
+from sensor.logger import logging
+import os 
+import sys
+
+def test_exception():
+    try:
+        logging.info("Division by zero here!")
+        a = 1/0
+    except Exception as e:
+        raise SensorException(e, sys)
+
+if __name__ == "__main__":
+    try:
+        test_exception()
+    except Exception as e:
+        print(e)
